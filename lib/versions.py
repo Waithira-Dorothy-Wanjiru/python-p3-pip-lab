3bacdd4e1acea2d6333f8714df32a01e8e18f1ab
Dorothy <dorothywanjiru32@gmail.com>
2025-08-28 09:39:14 +0300

    pip

--- lib/versions.py
+++ lib/versions.py
@@ -3,10 +3,13 @@ import requests
 import pytest
 
 def python_version():
+    """Return Python version info."""
     return sys.version_info
 
 def requests_version():
+    """Return installed requests version."""
     return requests.__version__
 
 def pytest_version():
+    """Return installed pytest version."""
     return pytest.__version__
